9abf254222745d5bfb9f64ddac56f6e7081ce1fc
Athesto <gamez.live@gmail.com>
2021-01-20 19:18:30 -0500

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from flask import Flask, request, render_template
+'''module with templates'''
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    '''home path'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''simple print'''
+    return "HBNB"
+
+
+@app.route('/c')
+@app.route('/c/<text>')
+@app.route('/python')
+@app.route('/python/<text>')
+def lang(text="is cool"):
+    '''lang'''
+    lang = request.path.split('/')[1]
+    return "{} {}".format(lang.capitalize(), text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''is a number'''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    '''template number page'''
+    page = "5-number.html"
+    return render_template(page, n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0', debug=False)
